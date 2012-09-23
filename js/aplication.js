9badd02527402fa82cdbe9dbb09c7a5bd3431ec1
Victor Baldin Gabetto <victor@bumpsolutions.com.br>
2012-09-22 21:50:52 -0300

    Estrutura geral

---
+++ js/aplication.js
@@ -0,0 +1,20 @@
+!function ($) {
+
+  $(function(){
+  
+  // tooltip demo
+    $('.tooltip-demo').tooltip({
+      selector: "a[rel=tooltip]"
+    })
+
+    $('.tooltip-test').tooltip()
+    $('.popover-test').popover()
+
+    // popover demo
+    $("a[rel=popover]")
+      .popover()
+      .click(function(e) {
+        e.preventDefault()
+      })
+      
+}}
